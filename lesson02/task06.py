0b2618ba450887ca1b561c65effc2332cf6f41e8
manchenkoff <artyom@manchenkoff.me>
2020-07-28 22:14:04 +0300

    lesson 02 solution

--- lesson02/task06.py
+++ lesson02/task06.py
@@ -1,7 +1,9 @@
 """
-* Реализовать структуру данных ​«​Товары​».​Она должна представлять собой список кортежей.
+* Реализовать структуру данных ​«​Товары​».​ Она должна представлять собой список кортежей.
 Каждый кортеж хранит информацию об отдельном товаре.
-В кортеже должно быть два элемента ​— номер товара и словарь с параметрами (характеристиками товара: название, цена, количество, единица измерения).
+В кортеже должно быть два элемента ​— номер товара и словарь с параметрами
+(характеристиками товара: название, цена, количество, единица измерения).
+
 Структуру нужно сформировать программно, т.е. запрашивать все данные у пользователя.
 
 Пример готовой структуры: [
@@ -24,3 +26,41 @@
     “ед”: [“шт.”]
 }
 """
+
+product_structure = {
+    "Название": str,
+    "Цена": int,
+    "Количество": int,
+    "Единица измерения": str,
+}
+
+product_list = []
+product_counter = 1
+
+while True:
+    decision = input(f"Товаров = {len(product_list)}, добавить? [y/n] ").lower()
+
+    if decision == 'n':
+        break
+    else:
+        product_info = {}
+
+        for property_name, property_type in product_structure.items():
+            user_input = input(f"Заполните поле '{property_name}' >>> ")
+            product_info[property_name] = property_type(user_input)
+            # product_info["Название"] = str("Холодильник")
+
+        product_list.append((product_counter, product_info))
+        product_counter += 1
+
+product_analytics = {}
+
+for analytics_key in product_structure.keys():
+    item_list = []
+
+    for product in product_list:
+        item_list.append(product[1][analytics_key])
+
+    product_analytics[analytics_key] = set(item_list)
+
+print(product_analytics)
