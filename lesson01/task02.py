eac4e7076da9271c2c94af7e3f74050a13a57e38
manchenkoff <artyom@manchenkoff.me>
2020-07-28 21:38:01 +0300

    lesson 01 solution

--- lesson01/task02.py
+++ lesson01/task02.py
@@ -3,3 +3,19 @@
 Переведите время в часы, минуты и секунды и выведите в формате чч:мм:сс.
 Используйте форматирование строк.
 """
+
+user_input = input("Введите время в секундах >>> ")
+
+if not user_input.isdigit():
+    print("Неверный формат числа")
+    exit()
+
+user_seconds = int(user_input)
+
+hours = user_seconds // 3600
+minutes = (user_seconds % 3600) // 60
+seconds = (user_seconds % 3600) % 60
+
+# hours, minutes, seconds = user_seconds // 3600, (user_seconds % 3600) // 60, (user_seconds % 3600) % 60
+
+print(f"{hours:>02}:{minutes:>02}:{seconds:>02}")
--- lesson01/task03.py
+++ lesson01/task03.py
@@ -2,3 +2,32 @@
 Узнайте у пользователя число n. Найдите сумму чисел n + nn + nnn.
 Например, пользователь ввёл число 3. Считаем 3 + 33 + 333 = 369.
 """
+
+user_input = input("Введите число >>> ")
+
+if not user_input.isdigit():
+    print("Неверный формат числа")
+    exit()
+
+# простой пример через диапазон (time => 3.5428000000115034)
+result = 0
+for x in range(1, 4):
+    result += int(user_input * x)
+
+print(result)
+
+# пример длины через while (time => 3.413199999990901)
+user_number = int(user_input)
+characters_count = 0
+temp_number = user_number
+
+while temp_number:
+    temp_number //= 10
+    characters_count += 1
+
+first_level_multiplication = (10 ** characters_count) + 1
+second_level_multiplication = (10 ** (characters_count * 2)) + first_level_multiplication
+
+result = user_number + (user_number * first_level_multiplication) + (user_number * second_level_multiplication)
+
+print(result)
